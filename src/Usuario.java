2d2ccfd2a1defc08e84f4c2a9f1ed6e860082b3a
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-04-01 11:37:09 -0300

    iniciando no POO

---
+++ src/Usuario.java
@@ -0,0 +1,8 @@
+// a classe/molde neste caso é Usuário
+public class Usuario {
+    //dentro da classe vai os atributos e características do obejeto correspondente
+    private String nome;
+    private String email;
+    private int nivelAcesso;
+    private boolean contaBloqueada;
+}

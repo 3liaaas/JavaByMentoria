33430242b9aad2cb3fff96930887944cf938613f
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-04-14 23:59:21 -0300

    praticando encapsulamento

--- src/PooBasico/ContaBancaria.java
+++ src/PooBasico/ContaBancaria.java
@@ -14,5 +14,15 @@ public class ContaBancaria {
     }
 
     // método com regra de negócio, não deixando setar o saldo diretamente
-    public boolean sacar
+    public boolean sacar(double valor){
+        if(valor <= 0 || valor > saldo) {
+            System.out.println("Saque inválido ou saldo insuficiente");
+            return false;
+        }
+        saldo -= valor;
+        return true;
+    }
 }
+
+ContaBancaria conta = new ContaBancaria("001-5", 1000.0);
+conta.sacar(250.0);
